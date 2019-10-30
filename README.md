f09c39a8e89f3dd46308ad9530829e7072c36c71
Sunlighter <edkiser@gmail.com>
2019-10-30 18:43:03 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,121 @@
+# Hiragana / Katakana Quiz Program in Gambit Scheme
+
+This is a simple quiz program I wrote in Gambit Scheme.
+
+The code can quiz on *any* subject, but I wrote it to quiz myself on the Japanese kana, so that is included. I got
+the [hiragana](https://en.wikipedia.org/wiki/Hiragana) and [katakana](https://en.wikipedia.org/wiki/Katakana) from
+their respective Wikipedia articles.
+
+The quiz program does *not* use [spaced repetition](https://en.wikipedia.org/wiki/Spaced_repetition). It just uses
+plain random numbers, which makes it as good as flash cards. Having hundreds of flash cards at a time is hard to
+learn, though, so I (sort of arbitrarily) divided the cards into *lessons.* You can quiz yourself on any one lesson or
+any combination of lessons.
+
+My intention was to start with a single lesson, master it, then move to the next one, master that, then try both
+together, and so forth.
+
+I am only just starting to quiz myself so I don&rsquo;t even know if I will successfully learn this way... but I
+figured it was worth a try.
+
+This program requires a terminal with UTF-8 support so that you can actually *see* the kana. I have set up vncserver
+in Amazon Linux 2, and the kana display just fine in the MATE terminal. I have not tried this in Windows, but it
+*might* work in the more modern Windows terminal.
+
+You use it like this:
+
+```
+gsi -:t8,f8
+(load "quiz.scm")
+(list-lessons) ; if you want to see them
+(define b (make-bank 'katakana-1)) ; or pick the lessons you want
+(randomize!)
+(run-quiz b 4) ; 4 is the number of wrong answers per question
+```
+
+I hope this program is useful, but I offer no warranty.
+
+Here are some notes on the program.
+
+`with-add` calls a procedure `proc` with an `add!` function. The procedure can loop and traverse and do whatever, and
+`add!` any items it chooses. When `proc` returns, the value it returns is discarded, but `with-add` returns the list
+of all the items that `proc` added.  Later I started passing `add-list!` to the procedure as well, so that it can add
+a list of items, all at once. I also started passing `items-so-far` so that the procedure could examine the list of
+items it had added up to that point.
+
+`for-each-lesson` calls a procedure `proc` for each defined lesson. `proc` is called as `(proc lesson-name
+lesson-items)`.
+
+`any` takes a `proc` and a list of `items` and returns `#t` if `proc` returns a true value for any of the `items`.
+
+`make-bank` takes zero or more lesson names and returns a vector of question-answer pairs. (It is not really useful to
+provide zero lesson names, but if you do, you will get an empty vector.)
+
+`for` implements a simple &ldquo;for&rdquo; loop.
+
+`lookup` takes a lesson name and a question or answer, and returns the corresponding answer or question, or `#f` if
+the question or answer is not found, or `#t` if the lesson is not found. (The `#f` and `#t` return values are not
+really useful.)
+
+`indistinguishable-items` is a variable constructed with `lookup`. I noticed that some kana look the same, or almost
+the same, and thought it would be &ldquo;unfair&rdquo; if one of them were the right answer and the other were
+presented as a wrong answer, so I created this variable.
+
+(Also, I wrote `lookup` because I myself can&rsquo;t type the kana, I have to paste them, and when they are
+indistinguishable, it is hard to tell if I even pasted the correct one. So I look them up...)
+
+`indistinguishable?` is a test to see if two symbols are indistinguishable. It uses `indistinguishable-items`. Also,
+two symbols that are `equal?` are considered indistinguishable.
+
+So this quiz program will never generate a question like this:
+
+```
+フ
+
+   0) fu
+   1) fu
+```
+
+This question might otherwise be generated if you combined `hiragana-3` and `katakana-3` in the same bank. One of the
+`fu` answers corresponds to `フ` and is correct. But the quiz program draws the wrong answers from other questions in
+the same bank. So the other `fu` corresponds to `ふ` and is incorrect &mdash; but there is no way for you to tell
+which `fu` is which. So I wrote code to make sure that no wrong answer is presented if it is `indistinguishable?` from
+the correct answer &mdash; or from any other wrong answer.  A check is also made to ensure that the *questions* are
+distinguishable, so you will also never see:
+
+```
+fu
+
+  0) フ
+  1) ふ
+```
+
+I did go ahead and include `っ` and `つ` as indistinguishable, and `ッ` and `ツ`, even though they are different sizes
+and so should be distinguishable after all. It&rsquo;s harder to see the size when the symbol is all by itself.
+
+`make-distinct` creates a list of distinct items. The `init` argument can add any initial items.  `count` is an
+integer indicating how many items to generate. `random-item` is a procedure which takes no arguments and returns a new
+proposed item for the list. `eq?` tests whether two items are &ldquo;equal.&rdquo;
+
+`shuffle!` randomly shuffles a vector.
+
+`find-index` returns the index of the first item in the vector `vec` to cause `proc` to return a true value. If there
+is no such item, `find-index` returns `#f`.
+
+`make-question` makes a question, drawn from a given bank, and with a given number of wrong answers. I suppose this
+function is a little inefficient, because it uses `shuffle!` to hide the right answer among the wrong answers, and
+then uses `find-index` to find it again after shuffling. Really, the wrong answers are already in random order, so a
+*full* shuffle is not necessary &mdash; and if the right answer is the only thing being moved, it should be much
+easier to know where it has been moved to. But I was lazy. (I did think of writing some kind of &ldquo;tracking
+shuffle&rdquo; so that you could track specific items in the shuffle and see where they had been moved to...)
+
+`randomize!` randomizes the random number generator in Gambit.
+
+`read-line-2` exists because, when you call `read` and read an item, any newline after the item is left in the input
+stream. The regular `read-line` function will read an empty line in that case. I want the user to enter something, so
+`read-line-2` reads a line and, if it is empty, reads *another* line. (Just once. It does not loop.)
+
+`run-quiz` is of course the primary function of this program. Mostly it is concerned with printing and getting input.
+
+In the distant future, I intend to add support for [kanji](https://en.wikipedia.org/wiki/Kanji). But those will be
+&ldquo;three-way&rdquo; questions &mdash; I will want the kanji, the pronunciation, and the meaning (in English).
+This will not be hard to add, but I want to learn the kana first...
---
+++ quiz.scm
@@ -0,0 +1,255 @@
+
+(define lessons '(
+    (katakana-1
+      (ア a) (イ i) (ウ u) (エ e) (オ o)
+      (カ ka) (キ ki) (ク ku) (ケ ke) (コ ko)
+      (ガ ga) (ギ gi) (グ gu) (ゲ ge) (ゴ go))
+    (katakana-2
+      (サ sa) (シ shi) (ス su) (セ se) (ソ so)
+      (ザ za) (ジ ji) (ズ zu) (ゼ ze) (ゾ zo)
+      (タ ta) (チ chi) (ツ tsu) (テ te) (ト to)
+      (ダ da) (ヂ dji) (ヅ dzu) (デ de) (ド do))
+    (katakana-3
+      (ナ na) (ニ ni) (ヌ nu) (ネ ne) (ノ no)
+      (ハ ha) (ヒ hi) (フ fu) (ヘ he) (ホ ho)
+      (バ ba) (ビ bi) (ブ bu) (ベ be) (ボ bo)
+      (パ pa) (ピ pi) (プ pu) (ペ pe) (ポ po))
+    (katakana-4
+      (マ ma) (ミ mi) (ム mu) (メ me) (モ mo)
+      (ヤ ya) (ユ yu) (ヨ yo)
+      (ラ ra) (リ ri) (ル ru) (レ re) (ロ ro)
+      (ワ wa) (ヲ wo)
+      (ン n))
+    (katakana-5
+      (キャ kya) (キュ kyu) (キョ kyo)
+      (シャ sha) (シュ shu) (ショ sho)
+      (チャ cha) (チュ chu) (チョ cho)
+      (ニャ nya) (ニュ nyu) (ニョ nyo))
+    (katakana-6
+      (ヒャ hya) (ヒュ hyu) (ヒョ hyo)
+      (ミャ mya) (ミュ myu) (ミョ myo)
+      (リャ rya) (リュ ryu) (リョ ryo)
+      (ー long-previous-vowel)
+      (ッ double-next-consonant) (ヽ repeat-unvoiced) (ヾ repeat-voiced))
+    (katakana-7
+      (ギャ gya) (ギュ gyu) (ギョ gyo)
+      (ジャ ja) (ジュ ju) (ジョ jo)
+      (ヂャ ja) (ヂュ ju) (ヂョ jo)
+      (ビャ bya) (ビュ byu) (ビョ byo)
+      (ピャ pya) (ピュ pyu) (ピョ pyo))
+    (hiragana-1
+      (あ a) (い i) (う u) (え e) (お o)
+      (か ka) (き ki) (く ku) (け ke) (こ ko)
+      (が ga) (ぎ gi) (ぐ gu) (げ ge) (ご go))
+    (hiragana-2
+      (さ sa) (し shi) (す su) (せ se) (そ so)
+      (ざ za) (じ ji) (ず zu) (ぜ ze) (ぞ zo)
+      (た ta) (ち chi) (つ tsu) (て te) (と to)
+      (だ da) (ぢ dji) (づ dzu) (で de) (ど do))
+    (hiragana-3
+      (な na) (に ni) (ぬ nu) (ね ne) (の no)
+      (は ha) (ひ hi) (ふ fu) (へ he) (ほ ho)
+      (ば ba) (び bi) (ぶ bu) (べ be) (ぼ bo)
+      (ぱ pa) (ぴ pi) (ぷ pu) (ぺ pe) (ぽ po))
+    (hiragana-4
+      (ま ma) (み mi) (む mu) (め me) (も mo)
+      (や ya) (ゆ yu) (よ yo)
+      (ら ra) (り ri) (る ru) (れ re) (ろ ro)
+      (わ wa) (を wo)
+      (ん n))
+    (hiragana-5
+      (きゃ kya) (きゅ kyu) (きょ kyo)
+      (しゃ sha) (しゅ shu) (しょ sho)
+      (ちゃ cha) (ちゅ chu) (ちょ cho)
+      (にゃ nya) (にゅ nyu) (にょ nyo))
+    (hiragana-6
+      (ひゃ hya) (ひゅ hyu) (ひょ hyo)
+      (みゃ mya) (みゅ myu) (みょ myo)
+      (りゃ rya) (りゅ ryu) (りょ ryo)
+      (っ double-next-consonant) (ゝ repeat-unvoiced) (ゞ repeat-voiced))
+    (hiragana-7
+      (ぎゃ gya) (ぎゅ gyu) (ぎょ gyo)
+      (じゃ ja) (じゅ ju) (じょ jo)
+      (ぢゃ ja) (ぢゅ ju) (ぢょ jo)
+      (びゃ bya) (びゅ byu) (びょ byo)
+      (ぴゃ pya) (ぴゅ pyu) (ぴょ pyo))
+      ))
+
+(define with-add (lambda (proc)
+    (let* (
+        (results '())
+        (add! (lambda (i) (set! results (cons i results)) #t))
+        (add-list! (lambda (l)
+            (let loop ((l l))
+              (if (null? l) #t
+                (begin (add! (car l)) (loop (cdr l)))))))
+        (items-so-far (lambda () results)))
+      (proc add! add-list! items-so-far)
+      (reverse results))))
+
+(define for-each-lesson (lambda (proc)
+    (let loop ((lessons lessons))
+      (if (null? lessons) #t
+        (let* (
+            (lesson (car lessons))
+            (lesson-name (car lesson))
+            (lesson-items (cdr lesson))
+            (lessons (cdr lessons)))
+          (proc lesson-name lesson-items)
+          (loop lessons))))))
+
+(define list-lessons (lambda ()
+    (with-add (lambda (add! add-list! items-so-far)
+        (for-each-lesson (lambda (name items) (add! name)))))))
+
+(define any (lambda (proc items)
+    (let loop ((items items))
+      (cond
+        ((null? items) #f)
+        ((proc (car items)) #t)
+        (else (loop (cdr items)))))))
+        
+(define make-bank (lambda lesson-names
+    (list->vector
+      (with-add (lambda (add! add-list! items-so-far)
+          (for-each-lesson (lambda (lesson-name lesson-items)
+              (if (any (lambda (n) (eq? n lesson-name)) lesson-names)
+                (add-list! lesson-items)))))))))
+
+(define for (lambda (s e proc)
+    (let loop ((i s))
+      (if (< i e) (begin (proc i) (loop (+ i 1)))
+        #t))))
+
+(define lookup (lambda (desired-lesson-name item)
+    (call-with-current-continuation (lambda (return)
+        (for-each-lesson (lambda (lesson-name lesson-items)
+            (if (eq? desired-lesson-name lesson-name)
+              (let loop ((li lesson-items))
+                (cond
+                  ((equal? (caar li) item) (return (cadar li)))
+                  ((equal? (cadar li) item) (return (caar li)))
+                  ((null? (cdr li)) (return #f))
+                  (else (loop (cdr li)))))
+              #t)))))))
+
+(define indistinguishable-items `(
+    (,(lookup 'katakana-3 'he) . ,(lookup 'hiragana-3 'he))
+    (,(lookup 'katakana-3 'be) . ,(lookup 'hiragana-3 'be))
+    (,(lookup 'katakana-3 'pe) . ,(lookup 'hiragana-3 'pe))
+    (,(lookup 'hiragana-6 'double-next-consonant) . ,(lookup 'hiragana-2 'tsu))
+    (,(lookup 'katakana-6 'double-next-consonant) . ,(lookup 'katakana-2 'tsu))))
+
+(define indistinguishable? (lambda (a b)
+    (or
+      (equal? a b)
+      (any
+        (lambda (p)
+          (or
+            (and (equal? a (car p)) (equal? b (cdr p)))
+            (and (equal? b (car p)) (equal? a (cdr p)))))
+        indistinguishable-items))))
+          
+(define make-distinct (lambda (init count random-item eq?)
+    (with-add (lambda (add! add-list! items-so-far)
+        (init add!)
+        (let loop ((count count))
+          (if (= count 0) #t
+            (let loop2 ()
+              (let ((candidate (random-item)))
+                (let loop3 ((items (items-so-far)))
+                  (cond
+                    ((null? items) (add! candidate) (loop (- count 1)))
+                    ((eq? (car items) candidate) (loop2))
+                    (else (loop3 (cdr items)))))))))))))
+
+(define shuffle! (lambda (vec)
+    (let (
+        (swap! (lambda (i j)
+            (if (= i j) #t
+              (let (
+                  (ii (vector-ref vec i))
+                  (jj (vector-ref vec j)))
+                (vector-set! vec i jj)
+                (vector-set! vec j ii)
+                #t)))))
+      (let loop ((c (vector-length vec)))
+        (if (<= c 0) vec
+          (let (
+              (i (random-integer c))
+              (j (- c 1)))
+            (swap! i j)
+            (loop j)))))))
+
+(define find-index (lambda (proc vec)
+    (let loop ((i 0))
+      (if (< i (vector-length vec))
+        (if (proc (vector-ref vec i))
+          i
+          (loop (+ i 1)))
+        #f))))
+
+(define make-question (lambda (bank wrong-count)
+    (let* (
+        (flip (random-integer 2))
+        (get-q (lambda (i) ((if (= flip 0) car cadr) (vector-ref bank i))))
+        (get-a (lambda (i) ((if (= flip 0) cadr car) (vector-ref bank i))))
+        (unfair? (lambda (i j)
+            (or
+              (indistinguishable? (get-q i) (get-q j))
+              (indistinguishable? (get-a i) (get-a j)))))
+        (bank-length (vector-length bank))
+        (item-index (random-integer bank-length))
+        (options (make-distinct
+            (lambda (add!) (add! item-index))
+            wrong-count
+            (lambda () (random-integer bank-length))
+            unfair?))
+        (q (get-q item-index))
+        (a-indexes (shuffle! (list->vector options)))
+        (a-values (list->vector (map get-a (vector->list a-indexes))))
+        (a-pick (find-index (lambda (i) (= i item-index)) a-indexes)))
+      `(,q ,a-values ,a-pick))))
+
+(define randomize! (lambda () (random-source-randomize! default-random-source)))
+
+(define read-line-2 (lambda ()
+    (let ((l (read-line)))
+      (if (zero? (string-length l)) (read-line) l))))
+      
+(define run-quiz (lambda (bank wrong-count)
+    (let loop ((do-prompt #f))
+      (let* (
+          (qdata (make-question bank wrong-count))
+          (q (car qdata))
+          (a-values (cadr qdata))
+          (a-pick (caddr qdata)))
+        (let (
+            (present-question (lambda ()
+                (shell-command "clear")
+                (display q) (newline) (newline)
+                (for 0 (vector-length a-values) (lambda (i)
+                    (display "  ")
+                    (display i)
+                    (display ") ")
+                    (display (vector-ref a-values i))
+                    (newline)))
+                (newline)
+                (display "Enter your answer or q to quit.")
+                (newline)
+                (display "? ")
+                (let ((a (read)))
+                  (cond
+                    ((equal? a 'q) #t)
+                    ((equal? a a-pick) (display "Correct!") (newline) (newline) (loop #t))
+                    (else (display "Wrong - it was ") (display a-pick) (display ".") (newline) (newline) (loop #t)))))))
+          (if do-prompt
+            (begin
+              (display "Enter q to quit or press enter to continue.")
+              (newline)
+              (display "? ")
+              (let ((b (read-line-2)))
+                (if (equal? b "q") #t
+                  (present-question))))
+            (present-question)))))))
+                
